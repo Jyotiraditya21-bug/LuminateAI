6b8404db535f9d6ffe9c2eaaec7dc1e56a7e3fe9
Jyotiraditya21-bug <jimmycodes2110@gmail.com>
2026-06-22 18:59:09 +0530

    Deploy update

---
+++ _next/static/txD5AALJvWIyjMU0tHkpS/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/LuminateAI//_next/:path+",
+        "destination": "/LuminateAI/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
